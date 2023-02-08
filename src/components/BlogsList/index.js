ce5bb20065fcd2b7cecae6f533ba6874f43a4ff4
Jinethmopada <jinethmopada@gmail.com>
2023-02-08 08:53:32 +0000

    code

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,45 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {BlogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      author: eachItem.author,
+      avatarUrl: eachItem.avatar_url,
+      topic: eachItem.topic,
+    }))
+    this.setState({BlogsData: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {BlogsData} = this.state
+    const {isLoading} = this.state
+
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          BlogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
